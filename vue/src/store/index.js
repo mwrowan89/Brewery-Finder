68c0c73157854326b53fec22af384f9bf6c7d891
mwrowan89 <138826237+mwrowan89@users.noreply.github.com>
2024-04-25 15:39:55 -0600

    Updated store

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,29 +1,38 @@
-import { createStore as _createStore } from 'vuex';
-import axios from 'axios';
+import { createStore as _createStore } from "vuex";
+import axios from "axios";
 
 export function createStore(currentToken, currentUser) {
   let store = _createStore({
     state: {
-      token: currentToken || '',
-      user: currentUser || {}
+      token: currentToken || "",
+      user: currentUser || {},
+      favoritedBreweries: [],
     },
     mutations: {
       SET_AUTH_TOKEN(state, token) {
         state.token = token;
-        localStorage.setItem('token', token);
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        localStorage.setItem("token", token);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       },
       SET_USER(state, user) {
         state.user = user;
-        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem("user", JSON.stringify(user));
       },
       LOGOUT(state) {
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        state.token = '';
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        state.token = "";
         state.user = {};
         axios.defaults.headers.common = {};
-      }
+      },
+      ADD_FAVORITED(state, favoriteBreweryToChange) {
+        // TODO: add brewery from favorites list
+        state.favoritedBreweries.unshift(favoriteBreweryToChange);
+      },
+      REMOVE_FAVORITED(state, indexToRemove) {
+        // TODO: remove brewery from favorites list
+        delete state.favoritedBreweries[indexToRemove];
+      },
     },
   });
   return store;
