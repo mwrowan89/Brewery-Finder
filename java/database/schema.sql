24cb7af0f136c4796b8524635351ca94592b6d04
mwrowan89 <138826237+mwrowan89@users.noreply.github.com>
2024-04-25 15:21:32 -0600

    Added controller

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, breweries, beers, reviews, favorite_breweries CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +9,56 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE breweries (
+    brewery_id SERIAL,
+    name VARCHAR(50),
+    phone varchar(255),
+    address varchar(255),
+	zip_code varchar(255),
+	state varchar(255),
+	city varchar(255),
+    history TEXT,
+    hours varchar(255),
+    image varchar(55555),
+	website_url varchar(255),
+	rating DECIMAL(3,1),
+	CONSTRAINT PK_brewery PRIMARY KEY (brewery_id)
+);
+
+CREATE TABLE beers (
+	beer_id SERIAL,
+	name VARCHAR(500),
+	brewery_id INT,
+	type VARCHAR(500),
+	abv DECIMAL(3, 1),
+	ibu INT,
+	description TEXT,
+	image VARCHAR(255),
+	show BOOLEAN DEFAULT true,
+	CONSTRAINT PK_beer PRIMARY KEY (beer_id),
+	FOREIGN KEY (brewery_id) REFERENCES breweries(brewery_id)
+);
+
+CREATE TABLE reviews (
+	review_id SERIAL,
+	beer_id INT,
+	user_id INT,
+	rating DOUBLE PRECISION,
+	review TEXT,
+	CONSTRAINT PK_review PRIMARY KEY (review_id),
+	FOREIGN KEY (beer_id) REFERENCES beers(beer_id),
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE favorite_breweries(
+    user_id INT,
+    brewery_id INT,
+    notes TEXT,
+    CONSTRAINT PK_favorite_breweries PRIMARY KEY (user_id, brewery_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (brewery_id) REFERENCES breweries(brewery_id)
+);
+
 
 COMMIT TRANSACTION;
---
+++ java/src/main/java/com/techelevator/controller/BreweryController.java
@@ -0,0 +1,123 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.BeerInfoDao;
+import com.techelevator.dao.BeerReviewDao;
+import com.techelevator.dao.BreweryInfoDao;
+import com.techelevator.model.BeerInfo;
+import com.techelevator.model.BeerReview;
+import com.techelevator.model.BreweryInfo;
+import com.techelevator.model.FavoriteBrewery;
+import com.techelevator.services.BreweryService;
+import com.techelevator.dao.UserDao;
+
+import java.util.List;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class BreweryController {
+
+    private BreweryInfoDao breweryInfoDao;
+    private BreweryService breweryService;
+    private BeerInfoDao beerInfoDao;
+    private UserDao userDao;
+    private BeerReviewDao beerReviewDao;
+
+    public BreweryController(BreweryInfoDao breweryInfoDao, BreweryService breweryService, UserDao userDao, BeerInfoDao beerInfoDao, BeerReviewDao beerReviewDao){
+        this.breweryInfoDao = breweryInfoDao;
+        this.breweryService = breweryService;
+        this.userDao = userDao;
+        this.beerInfoDao = beerInfoDao;
+        this.beerReviewDao = beerReviewDao;
+    }
+
+    @RequestMapping (path = "/", method = RequestMethod.GET)
+    public List<BreweryInfo> getAllBreweries() {
+        return breweryInfoDao.getAllBreweries();
+    }
+
+    @RequestMapping (path = "/brewery/{id}", method = RequestMethod.GET)
+    public BreweryInfo getBreweryById(@PathVariable int id) {
+        BreweryInfo breweryInfo = breweryInfoDao.getBreweryById(id);
+        return breweryInfo;
+    }
+    @RequestMapping (path = "/favorite/add", method = RequestMethod.POST)
+    public void addFavoriteBrewery(@RequestBody FavoriteBrewery favoriteBrewery) {
+        userDao.addFavoriteBrewery(favoriteBrewery);
+    }
+
+    @RequestMapping (path = "/favorite/delete", method = RequestMethod.DELETE)
+    public void removeFavoriteBrewery(@RequestBody FavoriteBrewery favoriteBrewery) {
+        userDao.removeFavoriteBrewery(favoriteBrewery);
+    }
+    @RequestMapping (path = "/beers/{id}", method = RequestMethod.GET)
+    public List<BeerInfo> getBeerListByBreweryId(@PathVariable int id) {
+        return beerInfoDao.getBeerListByBreweryId(id);
+    }
+
+    @RequestMapping (path = "/beers", method = RequestMethod.GET)
+    public List<BeerInfo> getAllBeers() {
+        return beerInfoDao.getAllBeers();
+    }
+//    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping (path = "/add/beers", method = RequestMethod.POST)
+    public void addBeer(@RequestBody BeerInfo beerInfo) {
+        beerInfoDao.addBeer(beerInfo);
+    }
+    @RequestMapping(path = "/beer/{id}/update", method = RequestMethod.PUT)
+    public void updateBeer(@RequestBody BeerInfo beerInfo, @PathVariable int id) {
+        beerInfoDao.updateBeer(beerInfo, id);
+    }
+    @RequestMapping (path = "/favoriteList/{id}", method = RequestMethod.GET)
+    public List<Integer> getFavoriteBreweries(@PathVariable int id) {
+        return userDao.getFavoriteBreweries(id);
+    }
+    @RequestMapping (path = "/beer/{id}", method = RequestMethod.GET)
+    public BeerInfo getBeerById(@PathVariable int id) {
+        return beerInfoDao.getBeerById(id);
+    }
+    @RequestMapping (path = "/add/review", method = RequestMethod.POST)
+    public void addReview(@RequestBody BeerReview beerReview) {
+        beerReviewDao.addReview(beerReview);
+    }
+    @RequestMapping (path = "/beer/{id}/reviews", method = RequestMethod.GET)
+    public List<BeerReview> getReviewsByBeerId(@PathVariable int id) {
+        return beerReviewDao.getReviewsByBeerId(id);
+    }
+    @RequestMapping (path = "/removeBeer/{id}", method = RequestMethod.PUT)
+    public BeerInfo updateBeerVisibility(@PathVariable int id) {
+        return beerInfoDao.hideBeer(id);
+    }
+    @RequestMapping (path = "/showBeer/{id}", method = RequestMethod.PUT)
+    public BeerInfo showBeer(@PathVariable int id) {
+        return beerInfoDao.showBeer(id);
+    }
+    @RequestMapping (path = "/update/favoritebrewery", method = RequestMethod.PUT)
+    public void updateFavBreweryNotes(@RequestBody FavoriteBrewery favoriteBrewery) {
+        userDao.updateFavBreweryNotes(favoriteBrewery);
+    }
+
+    @RequestMapping (path = "/favoritebrewery", method = RequestMethod.GET)
+    public List<FavoriteBrewery> getNotesByBreweryId() {
+        return userDao.getAllFavBreweries();
+    }
+    @RequestMapping (path = "/beers/average", method = RequestMethod.GET)
+    public List<BeerReview> averageRatingByBeer() {
+        return beerReviewDao.averageRatingByBeer();
+    }
+
+    @RequestMapping (path = "/brewery/{id}/update", method = RequestMethod.PUT)
+    public void updateBrewery(@RequestBody BreweryInfo breweryInfo, @PathVariable int id) {
+        breweryInfoDao.updateBrewery(breweryInfo, id);
+    }
+    @RequestMapping (path = "/admin/userList/reviews", method = RequestMethod.GET)
+    public List<BeerReview> getReviewsByUserId() {
+        return beerReviewDao.getAllReviews();
+    }
+    @RequestMapping(path = "/admin/userList/deleteReview/{id}", method = RequestMethod.DELETE)
+    public void deleteReview(@PathVariable int id) {
+        beerReviewDao.deleteReview(id);
+    }
+}
