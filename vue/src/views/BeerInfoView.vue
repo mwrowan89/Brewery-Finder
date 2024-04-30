155ba81f7e8550e07ecc722131953eef9e4ec68d
mwrowan89 <mwrowan89@gmail.com>
2024-04-30 15:56:07 -0600

    Added background to beer info

--- vue/src/views/BeerInfoView.vue
+++ vue/src/views/BeerInfoView.vue
@@ -1,4 +1,6 @@
 <template>
+  <body class="main">
+    <img src="https://teachrare.org/wp-content/uploads/2023/12/rare-disease-beer.webp" alt="retro beer">
     <div class="beerInfo">
       <h1 id="beer-name">{{ filteredBeer.beerName }}</h1>
       <p>
@@ -9,6 +11,7 @@
       <br />
       <ListOfReviews></ListOfReviews>
     </div>
+    </body>
   </template>
   
   <script>
@@ -51,15 +54,33 @@
   </script>
   
   <style scoped>
+  .main {
+    background-color: rgba(214, 191, 147, 0.491);
+    height: 100vh;
+    width: 100vw;
+    margin: 0; 
+    padding: 0; 
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+  img {
+    z-index: -1;
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+  }
   .beerInfo {
     display: flex;
     flex-direction: column;
-    margin-top: 100px;
+    margin-top: 7vw;
     margin-left: 3vw;
     padding: 20px;
     border: 1px solid black;
     border-radius: 10px;
-    background-color: rgba(255, 230, 172, 0.472);
+    background-color: rgba(0, 0, 0, 0.801);
     width: fit-content;
     max-width: 40vw;
     contain: content;
@@ -74,7 +95,7 @@
     font-size: 30px;
     margin-bottom: 10px;
     text-decoration: underline;
-    color: rgb(231, 224, 212);
+    color: rgb(250, 250, 250);
   }
   </style>
   
