4d47b82c351189ec2c7002fbd9ec332e0325d518
mwrowan89 <mwrowan89@gmail.com>
2024-10-15 10:47:10 -0600

    Updated table names in database

--- java/database/user.sql
+++ java/database/user.sql
@@ -2,24 +2,24 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER final_capstone_owner
-WITH PASSWORD 'finalcapstone';
+CREATE USER ale_atlas_owner
+WITH PASSWORD '1234';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_owner;
+TO ale_atlas_owner;
 
 GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_owner;
+TO ale_atlas_owner;
 
-CREATE USER final_capstone_appuser
-WITH PASSWORD 'finalcapstone';
+CREATE USER ale_atlas_appuser
+WITH PASSWORD '1234';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_appuser;
+TO ale_atlas_appuser;
 
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_appuser;
+TO ale_atlas_appuser;
